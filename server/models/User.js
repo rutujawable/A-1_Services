d2005e0c17951375d08f57040fb31b5701dbbf7b
bhushanpotdar19 <bhushanpotdar3@gmail.com>
2024-08-20 12:15:46 +0530

    controllers added

---
+++ server/models/User.js
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+
+    },
+    address: {
+        type: String,
+        required: true
+
+    },
+    mobile: {
+        type: Number,
+        required: true
+    },
+    age: {
+        type: Number,
+    }
+
+
+
+}, {
+    timestamps: true
+})
+const User = model('User', userSchema)
+export default User;
