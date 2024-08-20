b9d65d23c315c9a321805c39c8cc893c49584d49
bhushanpotdar19 <bhushanpotdar3@gmail.com>
2024-08-20 15:53:05 +0530

    index ready

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter,RouterProvider } from 'react-router-dom'
+import Home from './views/Home/Home';
+import Login from './views/Login/Login';
+import Signup from './views/Signup/Signup';
+import Category from './views/Category/Category'
+import Service from './views/Service/Service'
+import ServiceProvider from './views/SeviceProvider/ServiceProvider'
+import SlotBooking from './views/SlotBooking/SlotBooking'
+
+const router = createBrowserRouter([
+  {
+  path: '/',
+  element: <Home/>
+},{
+  path: '/category',
+  element: <Category/>
+},
+{
+  path: '/login',
+  element: <Login/>
+},
+{
+  path: '/service',
+  element: <Service/>
+},
+{
+  path: '/serviceprovider',
+  element: <ServiceProvider/>
+},
+{
+  path: '/slotbooking',
+  element: <SlotBooking/>
+},
+{
+  path: '/signup',
+  element: <Signup/>
+}
+
+
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router}/>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
