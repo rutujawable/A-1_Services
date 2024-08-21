aae953d6e34ca6f525b2b933f34417c67cacec35
sanket7472 <sankettamnar2003@gmail.com>
2024-08-21 11:40:58 +0530

    conflict

--- server/controllers/slotBooking.js
+++ server/controllers/slotBooking.js
@@ -1,24 +1,51 @@
- import SlotBook from "../models/SlotBooking.js";
+import SlotBook from "../models/SlotBooking.js";
 
-  const postBookSlot = async (req , res)=>{
+const postBookSlot = async (req, res) => {
     const { bookingDate, serviceDate, userAddress } = req.body
 
-    const SlotBook = new slotBook ({
+    const SlotBook = new SlotBook({
         bookingDate: bookingDate,
         serviceDate: serviceDate,
         userAddress: userAddress
     })
 
-const bookslot = await SlotBook.save()
+    const bookslot = await SlotBook.save()
 
-if (bookslot)
-    res.status(201).json({
-        success : true,
-        message: "Slot booked successfully", 
-       data : bookslot
-     })
+    if (bookslot)
+        res.status(201).json({
+            success: true,
+            message: "Slot booked successfully",
+            data: bookslot
+        })
 
 
-  }
+}
 
-  export default postBookSlot;
+
+const getAllSlots = async (req, res) => {
+    const { id } = req.params.id
+    if (!id){
+        return res.status(400).json({
+          success: false,
+            message: "Please provide id"})
+        }
+    
+    const SlotBook = await SlotBook.find({ userId: id })
+    if (SlotBook) {
+        res.status(200).json({
+            success: true,
+            message: "Slots found successfully",
+            data: SlotBook
+        })
+    } else {
+        res.status(404).json({
+            success: false,
+            message: "No slots found",
+            data: {}
+        })
+
+
+
+    }}
+
+    export { postBookSlot , getAllSlots};
