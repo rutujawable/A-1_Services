0305c9be03cc7546315e986a74aeeaa5e6657878
sanket7472 <sankettamnar2003@gmail.com>
2024-08-21 15:55:01 +0530

    frontend started

--- client/src/views/SlotBooking/SlotBooking.js
+++ client/src/views/SlotBooking/SlotBooking.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function SlotBooking() {
+    const {Slot , setSolt} = useState([])
   return (
     <div>
         <h1>Book a slot Now....!</h1>
         <form>
             <input
             type='date'
-            placeholder=''
             />
+            <input
+            type='date'
+            placeholder='Servicce Date'
+            />
+            <input
+            type='text'
+            placeholder='Service description'
+            />
+
         </form>
+
     </div>
   )
 }
